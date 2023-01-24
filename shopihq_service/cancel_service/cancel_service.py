b8212cb447cabfb2cc725b30a391f27194bb51b1
Burkay Elbek <burkay@4alabs.io>
2023-01-24 22:50:52 +0300

    Comment line added

--- shopihq_service/cancel_service/cancel_service.py
+++ shopihq_service/cancel_service/cancel_service.py
@@ -3,17 +3,22 @@ from shopihq_service.settings import Settings as settings
 
 
 class ShopihqCancelService(object):
-    @staticmethod
-    def test_request():
-        response = requests.get('https://npiregistry.cms.hhs.gov/api/?version=2.1&number=1275768103')
-        return response
 
     @staticmethod
     def _get_url_with_endpoint(endpoint):
+        """
+        :param endpoint:
+        :return:
+        """
         shopi_url = getattr(settings, 'SHOPIHQ_URL')
         return f"{shopi_url}{endpoint}"
 
     def get_reasons(self, request):
+        """
+        Method: GET
+        :param request:
+        :return:
+        """
         path = self._get_url_with_endpoint('/Order/reasons')
         response = requests.get(url=path, params=request.query_params)
         if response.status_code == 200:
@@ -22,6 +27,23 @@ class ShopihqCancelService(object):
             return False
 
     def is_cancellable(self, order_number):
+        """
+        Method: GET
+        :param order_number:
+        :return:
+        """
         path = self._get_url_with_endpoint(f'/Order/isCancelable/{order_number}')
         response = requests.get(url=path)
         return response
+
+    def cancel_order(self):
+        pass
+
+    def is_draft_returnable(self):
+        pass
+
+    def create_draft_return_shipment(self):
+        pass
+
+    def shipment_availability(self):
+        pass
