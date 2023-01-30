cbe32781d172ea3f2a365972c0469e1b9a4d6411
Burkay Elbek <burkay@4alabs.io>
2023-01-30 15:09:58 +0300

    EnvAccessor and folder structed edited

--- shopihq_service/order/order_service.py
+++ shopihq_service/order/order_service.py
@@ -6,12 +6,13 @@ class ShopihqOrderService(object):
     def __init__(self):
         self.headers = {"Content-Type": "application/json"}
 
-    def get_reasons(self, request):
+    def get_reasons(self, request, env_var):
         """
         Method: GET
+        :param env_var:
         :param request: Ex: ?type=0&language=1
         :return:
         """
         path = get_url_with_endpoint('/Order/reasons')
-        response = requests.get(url=path, params=request.query_params)
+        response = requests.get(url=path, params=request.query_params, auth=env_var)
         return response
