a0be33efdf9537d8c8b5237340849de66dfb78c8
Burkay Elbek <burkay@4alabs.io>
2023-02-05 16:38:09 +0300

    Website language filter to search in url and envaccessor deleted

--- shopihq_service/order/order_service.py
+++ shopihq_service/order/order_service.py
@@ -1,4 +1,7 @@
 import requests
+import uuid
+import json
+import re
 from shopihq_service.utils import get_url_with_endpoint
 
 
@@ -13,8 +16,39 @@ class ShopihqOrderService(object):
         : type=0 -> Cancel, type=1 -> Refund, language=0 -> Turkish, language=1 -> English
         :return:
         """
+        data = []
         path = get_url_with_endpoint('/Order/reasons')
-        response = requests.get(url=path, params=request.query_params)
+        website_url = request.META.get('PATH_INFO')
+        match = re.search(r'/en/|/en', website_url)
+        if match:
+            # Website language is English
+            language = 1
+        else:
+            # Website language is default (Turkish)
+            language = 0
+        params = {'type': 0, 'language': language}
+        response = requests.get(url=path, params=params, auth=('7BB5FD4A','138BE40D'))
+        if response.status_code != 200:
+            raise Exception("Error: API returned status code {}".format(response.status_code))
+        response_json = json.loads(response.content.decode())
+        parsed_json = response_json.get('data').get('reasonList')
+        for index, res in enumerate(parsed_json, start=1):
+            response_data = {
+                "cancellation_type": "cancel",
+                "created_date": None,
+                "modified_date": None,
+                "extra_information_needed": None,
+                "id": res['reasonId'],
+                "is_active": True,
+                "order": index,
+                "subject": res['reason'],
+                "translations": None,
+                "uuid": str(uuid.uuid4())
+            }
+            data.append(response_data)
+        results = {"results": data}
+        response.status_code = 200
+        response._content = json.dumps(results).encode()
         return response
 
     def order_search(self, request):
--- shopihq_service/utils.py
+++ shopihq_service/utils.py
@@ -1,5 +1,4 @@
 from shopihq_service.settings import Settings as settings
-import os
 
 
 def get_url_with_endpoint(endpoint):
@@ -9,14 +8,3 @@ def get_url_with_endpoint(endpoint):
     """
     shopi_url = getattr(settings, 'SHOPIHQ_BACKEND_URL')
     return f"{shopi_url}{endpoint}"
-
-
-class EnvAccessor:
-    def __getattr__(self, name):
-        try:
-            return os.environ[name]
-        except KeyError:
-            raise AttributeError(f"Attribute '{name}' does not exist in the .env file")
-
-    def __setattr__(self, name, value):
-        os.environ[name] = value
