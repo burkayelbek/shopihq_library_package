fb8f77f7e2d3ea3c16fda2f32937d2174e916e16
Burkay Elbek <burkay@4alabs.io>
2023-02-06 14:34:48 +0300

    Auth Utils and order service updated

--- shopihq_service/order/order_service.py
+++ shopihq_service/order/order_service.py
@@ -3,11 +3,13 @@ import uuid
 import json
 import re
 from shopihq_service.utils import get_url_with_endpoint
-
+from shopihq_service.utils import BasicAuthUtils
 
 class ShopihqOrderService(object):
+    auth = BasicAuthUtils()
+
     def __init__(self):
-        self.headers = {"Content-Type": "application/json"}
+        self.headers = {"Content-Type": "application/json", "Authorization": self.auth.basic_auth()}
 
     def get_reasons(self, request):
         """
@@ -26,27 +28,31 @@ class ShopihqOrderService(object):
         else:
             # Website language is default (Turkish)
             language = 0
-        params = {'type': 0, 'language': language}
-        response = requests.get(url=path, params=params)
-        if response.status_code != 200:
-            raise Exception("Error: API returned status code {}".format(response.status_code))
-        response_json = json.loads(response.content.decode())
-        parsed_json = response_json.get('data').get('reasonList')
+        cancel_params = {'type': 0, 'language': language}
+        refund_params = {'type': 1, 'language': language}
+        cancel_response = requests.get(url=path, params=cancel_params, headers=self.headers)
+        refund_response = requests.get(url=path, params=refund_params, headers=self.headers)
+        if (cancel_response.status_code and refund_response.status_code) != 200:
+            raise Exception(
+                f"Error: API returned status code CancelAPI: {cancel_response.status_code} "
+                f"& REFUND API: {refund_response.status_code}")
+        response_json_cancel = json.loads(cancel_response.content.decode())
+        response_json_refund = json.loads(refund_response.content.decode())
+        parsed_json = response_json_cancel.get('data').get('reasonList') + response_json_refund.get('data').get(
+            'reasonList')
         for index, res in enumerate(parsed_json, start=1):
             response_data = {
-                "cancellation_type": "cancel",
-                "created_date": None,
-                "modified_date": None,
-                "extra_information_needed": None,
                 "id": res['reasonId'],
-                "is_active": True,
-                "order": index,
+                "cancellation_type": "cancel" if res in response_json_cancel.get('data').get(
+                    'reasonList') else "refund",
                 "subject": res['reason'],
-                "translations": None,
-                "uuid": str(uuid.uuid4())
+                "extra_information_needed": True if res.get("reasonId") == -1 else None,
+                "order": index,
+
             }
             data.append(response_data)
-        results = {"results": data}
+        results = {"count": data.__len__(), "results": data}
+        response = requests.Response()
         response.status_code = 200
         response._content = json.dumps(results).encode()
         return response
--- shopihq_service/utils.py
+++ shopihq_service/utils.py
@@ -1,4 +1,17 @@
+import hashlib
+import base64
 from shopihq_service.settings import Settings as settings
+
+
+class BasicAuthUtils:
+    @staticmethod
+    def _generate_token(username, password):
+        token = f"{username}:{password}".encode("utf-8")
+        encoded_token = base64.b64encode(token).decode("utf-8")
+        return encoded_token
+
+    def basic_auth(self, username, password):
+        return "Basic " + self._generate_token(username, password)
 
 
 def get_url_with_endpoint(endpoint):
