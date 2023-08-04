18ee24a08c6673c2e55a6764e7599f65718f7337
Burkay Elbek <burkay@4alabs.io>
2023-08-04 21:45:41 +0300

    cancel logic changed

--- shopihq_service/cancel/service.py
+++ shopihq_service/cancel/service.py
@@ -33,11 +33,12 @@ class ShopihqCancelService(object):
         :param request:
         :return:
         """
-        order_item_list = request["orderItems"]
-        orderitem_id_list = [str(orderitem.get("orderItemId", "")) for orderitem in order_item_list]
+        #order_item_list = request["orderItems"]
+        #orderitem_id_list = [str(orderitem.get("orderItemId", "")) for orderitem in order_item_list]
+
         payload = {
-            "orderId": request.get("orderId"),
-            "orderItemIds": orderitem_id_list
+            "orderId": request.get("order_id"),
+            "orderItemIds": [str(request.get("order_item"))]
         }
         path = get_url_with_endpoint('Return/isDraftReturnable')
         try:
@@ -55,9 +56,19 @@ class ShopihqCancelService(object):
         :param request:
         :return:
         """
-        request_data = request.data.get("cancel_order_items", {})
-        order_id = request_data.get("orderId", "")
-        order_items = request_data.get("orderItems", [])
+
+        """
+        {
+          order_item: productId,
+          reason: reason.value
+          cancellation_type: "cancel"/"refund"
+          description:: "xxxx"
+          order_id: "xxxxx"
+        }
+        """
+        order_id = request.data.get("order_id", "")
+        order_item_id = request.data.get("order_item", "")
+
 
         cancel_payload = {
             "orderId": order_id,
@@ -72,20 +83,82 @@ class ShopihqCancelService(object):
             "deliveryAddressforRejectedReturn": None
         }
 
+        cancellation_type = request.data.get("cancellation_type")
+        if cancellation_type == "cancel":
+            order_item_payload = {
+                "orderItemId": request.data.get("order_item", ""),
+                "customerReason": int(request.data.get("reason")),
+                "customerStatement": request.data.get("description") if int(request.data.get("reason")) == -1 else ""
+            }
+            cancel_payload["orderItems"].append(order_item_payload)
+        else:
+            order_item_payload = {
+                "orderItemExternalId": request.data.get("order_item", ""),
+                "customerReason": int(request.data.get("reason")),
+                "customerStatement": request.data.get("description") if int(request.data.get("reason")) == -1 else ""
+            }
+            return_payload["orderItemList"].append(order_item_payload)
+
+        if cancel_payload["orderItems"]:
+            response = self.is_cancellable(order_number=order_id)
+            response_json = json.loads(response.content.decode())
+            data = response_json.get("data", {}).get("cancelableModel", [])
+            is_cancellable = all(cancellable_status.get("isCancelable", False) for cancellable_status in data
+                                 if cancellable_status.get("orderItemId", "") == order_item_id)
+
+            if is_cancellable:
+                path = get_url_with_endpoint('/Order/cancelOrder')
+                response = requests.post(url=path, headers=self.headers, json=cancel_payload)
+                return response
+            return {}
+
+        if return_payload["orderItemList"]:
+            request = request.data
+            response = self.is_draft_returnable(request=request)
+            response_json = json.loads(response.content.decode())
+            data = response_json.get("data")
+            is_returnable = all(draft_status.get("isDraftReturnable", False) for draft_status in data
+                                if draft_status.get("orderItemExternalId") == order_item_id)
+
+            if is_returnable:
+                path = get_url_with_endpoint('/Return/createDraftReturnShipment')
+                response = requests.post(url=path, headers=self.headers, json=return_payload)
+                return response
+            return {}
+
+    def bulk_cancel_order(self, request):
+        #ToDo: Should test on real data.
+        request_data = request.data.get("cancel_order_items")
+        order_id = request_data[0].get("order_id", "")
+        order_items = request_data
+
+        cancel_payload = {
+            "orderId": request.data.get("order_id"),
+            "orderItems": []
+        }
+
+        return_payload = {
+            "orderId": request.data.get("order_id"),
+            "orderItemList": [],
+            "returnDestinationNodeId": None,
+            "shipmentProvider": "20",
+            "deliveryAddressforRejectedReturn": None
+        }
+
         for orderitem in order_items:
             cancellation_type = orderitem.get("cancellation_type")
             if cancellation_type == "cancel":
                 order_item_payload = {
-                    "orderItemId": orderitem.get("orderItemId", ""),
-                    "customerReason": int(orderitem["customerReason"]),
-                    "customerStatement": orderitem["description"] if int(orderitem["customerReason"]) == -1 else ""
+                    "orderItemId": orderitem.get("order_item", ""),
+                    "customerReason": int(orderitem["reason"]),
+                    "customerStatement": orderitem["description"] if int(orderitem["reason"]) == -1 else ""
                 }
                 cancel_payload["orderItems"].append(order_item_payload)
             else:
                 order_item_payload = {
-                    "orderItemExternalId": orderitem.get("orderItemId", ""),
-                    "customerReason": int(orderitem["customerReason"]),
-                    "customerStatement": orderitem["description"] if int(orderitem["customerReason"]) == -1 else ""
+                    "orderItemExternalId": orderitem.get("order_item", ""),
+                    "customerReason": int(orderitem["reason"]),
+                    "customerStatement": orderitem["description"] if int(orderitem["reason"]) == -1 else ""
                 }
                 return_payload["orderItemList"].append(order_item_payload)
 
