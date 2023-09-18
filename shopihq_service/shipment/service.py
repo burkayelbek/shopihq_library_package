67ff74925efa81a381a3a0f49a75f86a738842a6
Burkay Elbek <burkay@4alabs.io>
2023-09-18 10:32:42 +0300

    shipment method changed

--- shopihq_service/shipment/service.py
+++ shopihq_service/shipment/service.py
@@ -8,7 +8,8 @@ class ShopihqShipmentService(object):
     auth = BasicAuthUtils()
 
     def __init__(self, username, password):
-        self.headers = {"Content-Type": "application/json", "Authorization": self.auth.basic_auth(username=username, password=password)}
+        self.headers = {"Content-Type": "application/json",
+                        "Authorization": self.auth.basic_auth(username=username, password=password)}
 
     def shipment_availability(self, request):
         """
@@ -17,5 +18,35 @@ class ShopihqShipmentService(object):
         :return:
         """
         path = get_url_with_endpoint('Shipment/availability')
-        response = requests.post(url=path, headers=self.headers, data=json.dumps(request.data))
+        pre_order = request.get("pre_order", {})
+        shipping_address = pre_order.get("shipping_address", {})
+        city = shipping_address.get("city", {}).get("name", ("",))
+
+        if '-' in city:
+            city = city.split('-')[0].strip()
+
+        payload = {
+            "basketTotal": pre_order.get("total_amount", ""),
+            "orderItems": [
+                {
+                    "integrationId": item.get("product", {}).get("sku", ""),
+                    "quantity": item.get("quantity", 0),
+                    "price": item.get("total_amount", "")
+                }
+                for item in pre_order.get("basket", {}).get("basketitem_set", [])
+            ],
+            "customerInfo": {
+                "customerType": "",
+                "firstName": shipping_address.get("first_name", ""),
+                "lastName": shipping_address.get("last_name", ""),
+                "phoneNumber": shipping_address.get("phone_number", ""),
+                "email": shipping_address.get("email", ""),
+            },
+            "deliveryAddress": {
+                "countryName": shipping_address.get("country", {}).get("name", ""),
+                "cityName": city,
+                "townName": shipping_address.get("township", {}).get("name", ""),
+            }
+        }
+        response = requests.post(url=path, headers=self.headers, data=json.dumps(payload))
         return response
