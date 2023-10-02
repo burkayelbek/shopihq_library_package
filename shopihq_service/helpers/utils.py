b4b4aee860170b9e47ee63852b40b0f69d54ac82
Burkay Elbek <burkay@4alabs.io>
2023-10-02 13:44:59 +0300

    550 status set as 500 code

--- shopihq_service/helpers/utils.py
+++ shopihq_service/helpers/utils.py
@@ -37,6 +37,7 @@ def get_order_status_mapping(order_data):
         410: {"value": "450", "label": "Hazırlanıyor"},
         415: {"value": "500", "label": "Kargolandı"},
         425: {"value": "500", "label": "Kargolandı"},
+        550: {"value": "500", "label": "Kargolandı"},
         510: {"value": "450", "label": "Hazırlanıyor"},
         540: {"value": "550", "label": "Teslim Edildi"},
         50: {"value": "100", "label": "İptal Edildi"},
